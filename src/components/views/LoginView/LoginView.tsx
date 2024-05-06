0f26260c30c7f9743c492786d41423ef0d822d55
santillienzo <enzo135246@gmail.com>
2024-05-06 16:09:54 -0300

    feat: add background

--- src/components/views/LoginView/LoginView.tsx
+++ src/components/views/LoginView/LoginView.tsx
@@ -1,10 +1,12 @@
 import Login from '@components/feature/auth/Login/Login'
 import style from './LoginView.module.css'
+import BackgroundCube from '@components/common/BackgroundCube/BackgroundCube'
 
 const LoginView = () => {
   return (
     <div className={style.loginContainer}>
       <Login/>
+      <BackgroundCube/>
     </div>
   )
 }

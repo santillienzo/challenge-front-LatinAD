b1991fa461dea69a12894ca91eb7f6872c0c36e3
santillienzo <enzo135246@gmail.com>
2024-05-06 22:09:09 -0300

    style: fix latinad logo

--- src/components/feature/auth/Login/Login.tsx
+++ src/components/feature/auth/Login/Login.tsx
@@ -1,20 +1,22 @@
 import { Paper, Typography } from '@mui/material'
 import LoginIcon from '@mui/icons-material/Login';
 import logo from '@assets/logo.svg'
-import style from './Login.module.css'
+import styles from './Login.module.css'
 import LoginForm from '../LoginForm/LoginForm';
 
 //Login form container component
 const Login = () => {
     
     return (
-        <Paper className={style.loginFormContainer}>
-                <img src={logo} alt="Logo LatinAD" />
+        <Paper className={styles.loginFormContainer}>
+                <div className={styles.logoContainer}>
+                    <img src={logo} alt="Logo LatinAD" className={styles.logo}/>
+                </div>
                 <Typography 
                     variant='h5' 
                     component="h2"
                     fontWeight={500}
-                    className={style.loginTitle}
+                    className={styles.loginTitle}
                 >
                     <LoginIcon fontSize='small'/>
                     Accede a tu cuenta
